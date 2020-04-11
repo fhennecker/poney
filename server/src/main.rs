961f724c1b39bcbd9ce808ffb1e0c577108b81cb
iTitou <moiandme@gmail.com>
2020-04-11 18:47:52 +0200

    [add] poneyprotocol in main
    Signed-off-by: iTitou <moiandme@gmail.com>

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,6 +3,8 @@ use std::thread::spawn;
 use tungstenite::protocol::Message;
 use tungstenite::server::accept;
 
+pub mod poneyprotocol;
+
 #[cfg(debug_assertions)]
 const BIND_IP: &str = "127.0.0.1:9001";
 
@@ -10,16 +12,24 @@ const BIND_IP: &str = "127.0.0.1:9001";
 const BIND_IP: &str = "0.0.0.0:9001";
 
 fn main() {
+    use poneyprotocol::*;
+
     println!("Started server on {}", BIND_IP);
 
     let server = TcpListener::bind(BIND_IP).unwrap();
     for stream in server.incoming() {
         spawn(move || {
             let mut websocket = accept(stream.unwrap()).unwrap();
+
             // send fake teams message upfront for debug
-            let fake_available_teams_msg = Message::from(format!(
-                "{\"type\": \"available_teams\"{\"payload\": [\"chaussettes\", \"saucettes\"]}}"
-            ));
+            let msg = DownConnMsg::AvailableTeams {
+                teams: vec![
+                    "chaussettes".to_string(),
+                    "saucettes".to_string(),
+                ]
+            };
+            let serialized = serde_json::to_string(&msg).unwrap();
+            let fake_available_teams_msg = Message::from(serialized);
             websocket.write_message(fake_available_teams_msg).unwrap();
 
             loop {
