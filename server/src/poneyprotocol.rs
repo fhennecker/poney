9e4d19dee16f2835be91dc9ecf7d8a746842f7dc
iTitou <moiandme@gmail.com>
2020-04-11 18:42:07 +0200

    [add] poneyprotocol with serde + set default cargo app as server
    Signed-off-by: iTitou <moiandme@gmail.com>

---
+++ server/src/poneyprotocol.rs
@@ -0,0 +1,45 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Item {
+    name: String,
+    price: f64
+}
+
+// Downstream message (server->client) in the connection management context
+#[derive(Serialize, Debug)]
+#[serde(tag="type", content="payload", rename_all="snake_case")]
+pub enum DownConnMsg {
+    Welcome,
+    AvailableTeams {teams: Vec<String>},
+    JoinedGame,
+    PlayerConnected {username: String, team: String},
+    GameStarted {username: String},
+}
+
+// Upstream message (client->server) in the connection management context
+#[derive(Deserialize, Debug)]
+#[serde(tag="type", content="payload", rename_all="snake_case")]
+pub enum UpConnMsg {
+    JoinRequest {username: String, team: String},
+    GameStartRequest {username: String},
+}
+
+// Downstream message in a game context
+#[derive(Serialize, Debug)]
+#[serde(tag="type", content="payload", rename_all="snake_case")]
+pub enum DownGameMsg {
+    PoneyDragged {team: String},
+    ItemsAvailable {items: Vec<Item>},
+    BudgetUpdate {budget: f64},
+    ItemBuyConfirm {item: String},
+    PoneyPosition {position: f64},
+    GameOver {winner: usize},
+}
+
+// Upstream message in a game context
+#[derive(Deserialize, Debug)]
+#[serde(tag="type", content="payload", rename_all="snake_case")]
+pub enum UpGameMsg {
+    ItemBuyRequest {item: String},
+}
